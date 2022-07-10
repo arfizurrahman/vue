92c1a1caf632058b9ee4a3d43b11873cd8c64e5e
Mohammad Arfizur Rahman <arahman@numberskills.se>
2022-07-10 20:46:43 +0600

    Add contact coach functionality in coach finder project

--- main-project-course-finder/src/pages/coaches/CoachDetail.vue
+++ main-project-course-finder/src/pages/coaches/CoachDetail.vue
@@ -35,7 +35,7 @@ export default {
             return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
         },
         contactLink() {
-            return this.$route.path + '/' + this.id + '/contact';
+            return this.$route.path + '/contact';
         }
     },
     created() {
