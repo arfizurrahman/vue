42a3d10eeb48a6ef045dbe5888fab69ec3221331
Mohammad Arfizur Rahman <arahman@numberskills.se>
2022-07-14 19:25:38 +0600

    Add search and sort hook

--- options-to-composition-api-migration/src/components/users/UserList.vue
+++ options-to-composition-api-migration/src/components/users/UserList.vue
@@ -15,7 +15,9 @@
 
 <script>
 
-import { ref, computed, watch } from 'vue';
+import { toRefs } from 'vue';
+import useSearch from '../../hooks/search';
+import useSort from '../../hooks/sort';
 import UserItem from './UserItem.vue';
 
 export default {
@@ -26,63 +28,15 @@ export default {
   emits: ['list-projects'],
   setup(props) {
 
-    //Region Searching
-    const enteredSearchTerm = ref('');
-    const activeSearchTerm = ref('');
+    const { users } = toRefs(props)
 
-    const availableUsers = computed(function () {
-      let users = [];
-      if (activeSearchTerm.value) {
-        users = props.users.filter((usr) =>
-          usr.fullName.includes(activeSearchTerm.value)
-        );
-      } else if (props.users) {
-        users = props.users;
-      }
-      return users;
-    })
+    const { enteredSearchTerm, availableItems, updateSearch } = useSearch(users, 'fullName');
 
-    function updateSearch(val) {
-      enteredSearchTerm.value = val;
-    }
-
-    watch(enteredSearchTerm, function (newValue) {
-      setTimeout(() => {
-        if (newValue === enteredSearchTerm.value) {
-          activeSearchTerm.value = newValue;
-        }
-      }, 300);
-    })
-
-    // Region Sorting
-
-    const sorting = ref(null);
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableUsers.value;
-      }
-      return availableUsers.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    })
-
-    function sort(mode) {
-      sorting.value = mode;
-    }
+    const { displayedUsers, sort, sorting } = useSort(availableItems, 'fullName');
 
     return {
       enteredSearchTerm,
-      activeSearchTerm,
       sorting,
-      availableUsers,
       displayedUsers,
       updateSearch,
       sort
